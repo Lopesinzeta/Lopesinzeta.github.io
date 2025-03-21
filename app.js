37ed9420d215b05777afae3acf39867c0d41fd90
lope <lesone.x64@gmail.com>
2025-03-21 12:07:51 +0100

    prueba web con git

---
+++ app.js
@@ -0,0 +1,25 @@
+/*HARD MODE
+
+1. Parecido al ejercicio NORMAL-1, haz una función que le des como entrada que operación quieres realizar (sumar, multiplicar, dividir, potencia, raíz,…) y los valores y que
+devuelva la soluciones, si la opción que pones no existe o los valores introducidos no son números que siga preguntando hasta las 10 preguntas entonces que responda con un
+“Me he cansado de ti.” 
+    
+    -El resultado deberá estar en una variable global
+    
+    -Todos los datos deberán ser introducidos por el usuario
+    
+    -Se supone que no vamos a introducir valores nulos
+    
+    -Suponemos que para dividir no dividimos por 0 (Esto los avispados podrán arreglarlo)
+    
+    -La salida deberá estar en una tabla que aparezca en nuestra página con este aspecto.
+    
+    -Extra: Si no pones el dato hasta el final que indique con una lista ordenada el número de error y los datos que se han introducido mal.*/
+/*
+        OPERACION   VARIABLE 1  VARIABLE 2  VARIABLE 3
+        sumar       3           4           7
+*/
+
+
+//2. Crear un programa JavaScript que simule el lanzamiento aleatorio de un dado usando las imágenes disponibles en la carpeta dado
+
---
+++ generador.js
@@ -0,0 +1,14 @@
+//Interacción con HTML (Pensad un poco)
+
+/*1. Realizar un código JavaScript que pida un texto y una dirección web mediante 2 prompt distintos, posteriormente crear un enlace (etiqueta <a>) que vaya a la dirección elegida
+por el usuario en una pestaña nueva. El texto del enlace debe ser el que se pidió al principio.*/
+
+/*2. Crear en JavaScript un programa que pida la localización de un archivo de imagen (mediante una URL de internet) y una dirección web (URL). Escribir una imagen
+centrandola con un parrafo que sea un enlace a la dirección web escrita.*/
+
+/*3. Crear un programa JavaScript que mediante el siguiente array 
+
+let datos= ["agua","zumo","pepsi","cocacola","sprite"];
+
+crea un select (botón desplegable) con opciones que serán las palabras del array. El
+atributo value coincide con el valor de la opción*/
